a7bd1c207d9791651f22f54f36d5ea08154f499c
Prasad Sutar <prasad.sutar@vandanam.co.in>
2025-01-12 03:39:55 +0530

    cost table logic add

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,7 +1,7 @@
 import { QuotationData,District,Taluka,Village } from '../types/quotation';
 
 
-const API_BASE_URL = 'http://192.168.1.5:7575/api';
+const API_BASE_URL = 'http://localhost:7575/api';
 
 export const generateQuotationPDF = async (data: QuotationData): Promise<Blob> => {
   try {
@@ -78,7 +78,7 @@ export const fetchPanelWattages = async (phase: string): Promise<number[]> => {
 
 //api for calculate kw using enrgy usage and phase type
 export const calculateKw = async (phase: string, energyUsage: number): Promise<number | null> => {
-  const url = 'http://192.168.1.5:7575/api/kw/calculate';
+  const url = 'http://localhost:7575/api/kw/calculate';
   const requestPayload = {
     phase,
     energyUsage: energyUsage.toString(), // converting energyUsage to string as per backend requirements
