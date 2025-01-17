c6911858d8d30743602c17b5c6918b908b425f1a
Prasad Sutar <prasad.sutar@vandanam.co.in>
2025-01-17 09:50:58 +0530

    fixed navbar at quotationform screen

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,43 +1,47 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import { QuotationForm } from './components/QuotationForm';
 import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar';
 
-function App() {
+const AppContent: React.FC = () => {
+  const location = useLocation();
+
+  // Show Navbar only on specific routes
+  const showNavbar = location.pathname !== '/login';
+
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        {/* Navbar will be shown conditionally */}
+    <div className="min-h-screen bg-gray-50">
+      {/* Conditionally Render Navbar */}
+      {showNavbar && <Navbar />}
+
+      <div className="py-12">
         <Routes>
+          {/* Redirect to login */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          
+          {/* Login Page */}
+          <Route path="/login" element={<Login />} />
+          
+          {/* Protected Quotation Form */}
           <Route
             path="/quotationform"
             element={
-              !['/login'].includes(window.location.pathname) && <Navbar />
+              <PrivateRoute>
+                <QuotationForm />
+              </PrivateRoute>
             }
           />
         </Routes>
-
-        <div className="py-12">
-          <Routes>
-            {/* Redirect to login */}
-            <Route path="/" element={<Navigate to="/login" />} />
-            
-            {/* Login Page */}
-            <Route path="/login" element={<Login />} />
-            
-            {/* Protected Quotation Form */}
-            <Route
-              path="/quotationform"
-              element={
-                <PrivateRoute>
-                  <QuotationForm />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </div>
       </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
