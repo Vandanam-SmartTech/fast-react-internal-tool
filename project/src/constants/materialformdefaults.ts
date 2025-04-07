37a131fe040f24a22c1923314ff6fabb523c4325
dm-omkar-patil <omkar.patil@vandanam.co.in>
2025-04-07 16:12:26 +0530

    Form update

--- project/src/constants/materialformdefaults.ts
+++ project/src/constants/materialformdefaults.ts
@@ -1,24 +1,22 @@
 export const materialformDefaults = {
-  //  connectionId: number,
-    systemKw: "",
-    makeOfModule: "",
-    almmModelNo: "",
-    serialNoOfModules: "",
-    wattagePerModule: "",
-    noOfModules: "",
-    totalCapacity: "",
-    warrantyDetails: "",
-    inverterModuleNo: "",
-    inverterMake: "",
-    rating: "",
-    chargeControllerType: "",
-    inverterCapacity: "",
-    earthingRod: "",
-    dateOfInstallation: "",
-    capacityType: "",
-    projectModel: "",
-    reInstalledCapacityRooftop: "",
-    reInstalledCapacityGround: "",
-    reInstalledCapacityTotal: "",
-  };
-  
+  systemKw: 0,                              // BigDecimal
+  makeOfModule: "",                         // String
+  almmModelNo: "",                          // String
+  serialNoOfModules: "",                    // String
+  wattagePerModule: 0,                      // Integer
+  noOfModules: 0,                           // Integer
+  totalCapacity: 0,                         // BigDecimal
+  warrantyDetails: "",                      // String
+  inverterModuleNo: "",                     // String
+  inverterMake: "",                         // String
+  rating: "",                               // String
+  chargeControllerType: "",                 // String
+  inverterCapacity: 0,                      // BigDecimal
+  earthingRod: 0,                           // Integer
+  dateOfInstallation: "",                   // ISO date string: "YYYY-MM-DD"
+  capacityType: "",                         // String
+  projectModel: "",                         // String
+  reInstalledCapacityRooftop: 0,            // BigDecimal
+  reInstalledCapacityGround: 0,             // BigDecimal
+  reInstalledCapacityTotal: 0               // BigDecimal
+};
