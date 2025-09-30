9b9cc2071c3489f5da6a27f1a6612c1b851b941a
vstSamiksha <samiksha.devardekar@vandanam.co.in>
2025-09-30 12:56:42 +0530

    Change System-Specs Screen deploy-dev

--- src/pages/SystemSpecifications/SystemSpecifications.tsx
+++ src/pages/SystemSpecifications/SystemSpecifications.tsx
@@ -800,7 +800,6 @@ export const SystemSpecifications = () => {
             <h3 className="text-lg font-semibold text-gray-800">System Specifications</h3>
           </div>
 
-          {/* Right side: Button */}
           {savedSpecs.length > 0 && (<button
             type="button"
             className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white text-sm font-medium rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
