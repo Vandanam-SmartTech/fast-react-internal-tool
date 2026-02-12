237c6dc50fa870d9aef9e4802d0050e1410ce9f2
dm-omkar-patil <omkar.patil@vandanam.co.in>
2026-02-12 18:24:54 +0530

    Preview page intigration with APIs

--- src/pages/SystemSpecifications/ViewSystemSpecifications.tsx
+++ src/pages/SystemSpecifications/ViewSystemSpecifications.tsx
@@ -205,7 +205,7 @@ export const ViewSystemSpecifications = () => {
                         className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
                     >
                         <Plus className="w-4 h-4" />
-                        Add New Package
+                        Add New Solar Product
                     </button>
                 </div>
 
